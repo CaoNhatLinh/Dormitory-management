5db71863e6551c4f29cb900e35388fa89560a80c
NhatLinh <caonhatlinh1312@gmail.com>
2024-04-11 20:15:27 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Register
+                                            RegisteR
                                         </a>
                                     @endif
                                 @endauth
