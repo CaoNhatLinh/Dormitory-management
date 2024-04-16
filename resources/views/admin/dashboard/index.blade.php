12ba9815f3e80f5efb69f24adc07e1c44a0f4b70
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-04-16 14:45:42 +0700

    change link js file in dashboard page

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1287,43 +1287,44 @@
     </div>
 
     <!-- Mainly scripts -->
-    <script src="../../resources/js/jquery-3.1.1.min.js"></script>
-    <script src="../../resources/js/bootstrap.min.js"></script>
-    <script src="../../resources/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="../../resources/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+    <script src="{{asset('js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 
     <!-- Flot -->
-    <script src="../../resources/js/plugins/flot/jquery.flot.js"></script>
-    <script src="../../resources/js/plugins/flot/jquery.flot.tooltip.min.js"></script>
-    <script src="../../resources/js/plugins/flot/jquery.flot.spline.js"></script>
-    <script src="../../resources/js/plugins/flot/jquery.flot.resize.js"></script>
-    <script src="../../resources/js/plugins/flot/jquery.flot.pie.js"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.tooltip.min.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.spline.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/query.flot.resize.js')}}"></script>
+    <script src="{{asset('js/plugins/flot/jquery.flot.pie.js')}}"></script>
+
 
     <!-- Peity -->
-    <script src="../../resources/js/plugins/peity/jquery.peity.min.js"></script>
-    <script src="../../resources/js/demo/peity-demo.js"></script>
+    <script src="{{asset('js/plugins/peity/jquery.peity.min.js')}}"></script>
+    <script src="{{asset('js/demo/peity-demo.js')}}"></script>
 
     <!-- Custom and plugin javascript -->
-    <script src="../../resources/js/inspinia.js"></script>
-    <script src="../../resources/js/plugins/pace/pace.min.js"></script>
+    <script src="{{asset('js/inspinia.js')}}"></script>
+    <script src="{{asset('js/plugins/pace/pace.min.js')}}"></script>
 
     <!-- jQuery UI -->
-    <script src="../../resources/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="{{asset('js/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
 
     <!-- GITTER -->
-    <script src="../../resources/js/plugins/gritter/jquery.gritter.min.js"></script>
+    <script src="{{asset('js/plugins/gritter/jquery.gritter.min.js')}}"></script>
 
     <!-- Sparkline -->
-    <script src="../../resources/js/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <script src="{{asset('js/plugins/sparkline/jquery.sparkline.min.js')}}"></script>
 
     <!-- Sparkline demo data  -->
-    <script src="../../resources/js/demo/sparkline-demo.js"></script>
+    <script src="{{asset('js/demo/sparkline-demo.js')}}"></script>
 
     <!-- ChartJS-->
-    <script src="../../resources/js/plugins/chartJs/Chart.min.js"></script>
+    <script src="{{asset('js/plugins/chartJs/Chart.min.js')}}"></script>
 
     <!-- Toastr -->
-    <script src="../../resources/js/plugins/toastr/toastr.min.js"></script>
+    <script src="{{asset('js/plugins/toastr/toastr.min.js')}}"></script>
 
 
     <script>
