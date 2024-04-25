e7d2abab36bc47e4e957021b7ed00c9befa5003d
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-04-25 21:21:02 +0700

    format controller, route style

--- app/Http/Controllers/admin/User/AuthController.php
+++ app/Http/Controllers/admin/User/AuthController.php
@@ -11,41 +11,37 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        
     }
-   
-    public function index(){
-        if(Auth::id()>0)
-        {
-            return redirect()->route('dashboard.index')->with('succes','dăng nhập thành công');
+
+    public function index()
+    {
+        if (Auth::id() > 0) {
+            return redirect()->route('dashboard.index')->with('succes', 'dăng nhập thành công');
         }
-        
+
         return view('admin.user.auth.login');
-     } 
-    
-     public function login(AuthRequest $request){
+    }
+
+    public function login(AuthRequest $request)
+    {
         $credentials = [
             'email' => $request->input('email'),
             'password' => $request->input('password')
         ];
-        
-        if (Auth::attempt($credentials,$request-> input('remember'))) {
-            return redirect()->route('dashboard.index')->with('succes','Đăng nhập thành công');
-           
+
+        if (Auth::attempt($credentials, $request->input('remember'))) {
+            return redirect()->route('dashboard.index')->with('succes', 'Đăng nhập thành công');
         }
-        return redirect()->back()->with('error','email hoặc mật khẩu không chính xác');
-     } 
-     public function logout(Request $request)
-     {
-         Auth::logout();
-     
-         $request->session()->invalidate();
-     
-         $request->session()->regenerateToken();
-     
-         return redirect()->route('auth.admin');
-     } 
-        
-     
+        return redirect()->back()->with('error', 'email hoặc mật khẩu không chính xác');
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect()->route('auth.admin');
+    }
 }
-    
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Admin\User\DashboardController;
 use App\Http\Controllers\Admin\User\EmployeeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-Use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Auth\Middleware\Authenticate;
 
 
 Route::get('/', function () {
@@ -13,11 +13,11 @@ Route::get('/', function () {
 });
 
 
-Route::GET('admin',[AuthController::class ,'index' ])->name('auth.admin');
-Route::post('login',[AuthController::class ,'login' ])->name('auth.login');
-Route::get('logout',[AuthController::class ,'logout' ])->name('auth.logout');
+Route::GET('admin', [AuthController::class, 'index'])->name('auth.admin');
+Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-Route::GET('dashboard/index',[DashboardController::class ,'index' ])->name('dashboard.index') ->middleware('auth');
+Route::GET('dashboard/index', [DashboardController::class, 'index'])->name('dashboard.index')->middleware('auth');
 
 // EMPLOYEE
-Route::get('employee',[EmployeeController::class ,'index' ])->name('employee.index');
+Route::get('employee', [EmployeeController::class, 'index'])->name('employee.index');
