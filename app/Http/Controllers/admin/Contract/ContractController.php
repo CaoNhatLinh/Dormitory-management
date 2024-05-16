1bbd6242cbc69a5af3ec16cc0c3bd8b37d15aa61
Vinh Huynh <huynhthevinh1608@gmail.com>
2024-05-16 22:11:30 +0700

    refactor: add config to controllers

--- app/Http/Controllers/admin/Contract/ContractController.php
+++ app/Http/Controllers/admin/Contract/ContractController.php
@@ -19,6 +19,33 @@ class ContractController extends Controller
     {
     }
 
+    public function config()
+    {
+        return $config = [
+            'js' => [
+                'js/plugins/flot/jquery.flot.js',
+                'js/plugins/flot/jquery.flot.js',
+                'js/plugins/flot/jquery.flot.tooltip.min.js',
+                'js/plugins/flot/jquery.flot.spline.js',
+                'js/plugins/flot/query.flot.resize.js',
+                'js/plugins/flot/query.flot.resize.js',
+                'js/plugins/flot/jquery.flot.pie.js',
+                'js/plugins/peity/jquery.peity.min.js',
+                'js/demo/peity-demo.js',
+                'js/inspinia.js',
+                'js/plugins/gritter/jquery.gritter.min.js',
+                'js/demo/sparkline-demo.js',
+                'js/plugins/sparkline/jquery.sparkline.min.js',
+                'js/plugins/chartJs/Chart.min.js',
+                'js/plugins/toastr/toastr.min.js',
+
+            ],
+            'css' => [
+                'css/dashboard.css'
+            ]
+        ];
+    }
+
     public function index()
     {
         $contracts = Contract::all();
